a33ebc843db1701e81b441e7fee198dc8b8d7fe1
andrew.shine <andrew.shine@MU-C1-10.mullen.pvt.k12.co.us>
2019-02-04 08:43:31 -0700

    Made basic files

---
+++ src/SISRunner.java
@@ -0,0 +1,22 @@
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.filechooser.*;
+import java.util.ArrayList;
+import java.io.*;
+import java.nio.file.Files;
+import javax.swing.JTextArea;
+import java.util.ArrayList;
+
+@SuppressWarnings({ "unused", "serial" })
+public class SISRunner extends JPanel
+	{
+		static JPanel panel;
+		static JFrame frame;
+		
+		public static void main(String[] args)
+			{
+				//Initial commit
+			}
+
+	}
---
+++ src/Student.java
@@ -0,0 +1,41 @@
+import java.util.ArrayList;
+public class Student
+	{
+		private String name;
+		private double GPA;
+		private ArrayList<Course> classes;
+		
+		public Student(String n, double g)
+		{
+			name = n;
+			GPA = g;
+			classes = new ArrayList<Course>();
+		}
+
+		public String getName()
+			{
+				return name;
+			}
+		public void setName(String name)
+			{
+				this.name = name;
+			}
+
+		public double getGPA()
+			{
+				return GPA;
+			}
+		public void setGPA(double gPA)
+			{
+				GPA = gPA;
+			}
+
+		public ArrayList<Course> getClasses()
+			{
+				return classes;
+			}
+		public void setClasses(ArrayList<Course> classes)
+			{
+				this.classes = classes;
+			}
+	}
